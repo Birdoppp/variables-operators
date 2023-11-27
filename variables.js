aecd65706518f9e25bae49c34d35f2a71c843a01
Bridoppp <tini.pham99@gmail.com>
2023-11-27 09:54:40 +0100

    Variables layout fix

--- variables.js
+++ variables.js
@@ -15,7 +15,7 @@ console.log(name);
 // 2. Declareer een variabele om de kleur van jouw ogen in op te slaan en zet de waarde erin. De variabele hoeft niet meer overschreven hoeft te worden.
 // ==========================================
 const eyeColor = "Brown";
-console.log("Their eyecolor is "+ eyeColor);
+console.log("Their eyecolor is " + eyeColor);
 // ==========================================
 // 3. Declareer een variabele om jouw leeftijd in op te slaan en zet de waarde erin. De variabele moet later overschreven kunnen worden.
 // ==========================================
@@ -26,12 +26,12 @@ console.log("Their age is " + age)
 // 4. Declareer een variabele om jouw beroep in op te slaan en zet de waarde erin. De variabele moet later overschreven kunnen worden.
 // ==========================================
 let occupation = "student";
-console.log("Their current occupation is "+occupation);
+console.log("Their current occupation is " + occupation);
 
 // ==========================================
 // 5. Declareer een variabele om jouw schoenmaat in op te slaan en zet de waarde erin. De variabele hoeft niet meer overschreven hoeft te worden.
 // ==========================================
-const shoeSize= 36;
+const shoeSize = 36;
 console.log("they have shoe size " + shoeSize);
 
 // ==========================================
@@ -40,18 +40,16 @@ console.log("they have shoe size " + shoeSize);
 let partner = true;
 
 if (partner === true) {
-    console.log( "Has a partner");
+    console.log("Has a partner");
+} else {
+    console.log("Has no partner");
 }
-
-else {
-    console.log( "Has no partner");
-    }
 
 // ==========================================
 // 7. Declareer een variabele om het aantal kinderen in op te slaan. De variabele moet later overschreven kunnen worden.
 // ==========================================
-let children= 0;
-console.log("has "+ children + " children")
+let children = 0;
+console.log("has " + children + " children")
 // ==========================================
 // 8. Gebruik jouw variabelen nu om hun waardes te loggen in de terminal. Klopt het allemaal?
 // ==========================================
